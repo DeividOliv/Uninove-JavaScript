14a5569df1405673f2327108b900ac25736298a2
Deivid Oliveira <deividbenner17@gmail.com>
2022-05-31 20:29:50 -0300

    Métodos Array

---
+++ Aula 6/script6_06.js
@@ -0,0 +1,5 @@
+var frutas = ["Laranja", "Morango", "Melão", "Figo"];
+var junto = frutas.toString();
+//document.write(junto);
+document.querySelector("#dv").innerHTML=junto;
+//document.querySelector(".teste").innerHTML=junto;
