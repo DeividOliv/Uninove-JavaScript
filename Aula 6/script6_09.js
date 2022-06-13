861aca128abdadba641f6ffbc97b23725cba0a9e
Deivid Oliveira <deividbenner17@gmail.com>
2022-06-13 19:56:23 -0300

    Métodos push e unshift

---
+++ Aula 6/script6_09.js
@@ -0,0 +1,12 @@
+const id_dv=document.querySelector("#dv");
+const class_teste=document.querySelector(".teste");
+const id_nome=document.querySelector("#nome");
+const tag_h2=document.querySelector("h2");
+
+
+var login=["admin","root"];
+//document.write(login+"<hr>");
+login.push("astrogildo");
+//document.write(login+"<hr>");
+login.unshift("belinha");
+//document.write(login+"<hr>");
