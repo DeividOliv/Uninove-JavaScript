77e5bceb3ede1f64505b6bf1832c24ba1b62e62a
Deivid Oliveira <deividbenner17@gmail.com>
2022-06-14 21:11:14 -0300

    Criação de função

---
+++ Aula 7/script7_02.js
@@ -0,0 +1,16 @@
+function media(nota1,nota2,nota3){
+    return (nota1+nota2+nota3)/3;
+    //return "Eita!!!";
+}
+/* 
+usa o return dentro da função, o return devolve
+o valor para alguem
+*/
+
+var num;
+num = media(8,7,9);
+document.write ("Num = "+num+"<br>");
+document.write("Media Final: "+media(9,7,9));
+/* 
+Usa o comando de saida fora da função
+*/
